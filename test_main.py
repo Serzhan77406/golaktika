7f984e62731ee986a1e0a44afc03ebcefe8637c0
Serzhan77406 <Aigul.dauletyar@mail.ru>
2026-03-02 20:41:58 +0500

    MYsecond_CI/CD

--- test_main.py
+++ test_main.py
@@ -1,4 +1,4 @@
-from main import add
+from main import add, multiply
 
 def test_add():
     assert add(2, 3) == 5
@@ -6,5 +6,5 @@ def test_add():
 
 
 def test_multiply():
-    assert add(5, 3) == 15
-    assert add(0, 0) == 0    
+    assert multiply(5, 3) == 15
+    assert multiply(0, 0) == 0    
